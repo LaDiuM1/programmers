be6d7d8dae470168fee5023244dd2358f4c4ac04
LaDiuM <wjddhrtjd123@gmail.com>
2024-07-03 22:09:20 +0900

    [LEVEL2 - 광물 캐기] 해결

--- src/main/java/org/programmers/level2/Solution2ByLv2.java
+++ src/main/java/org/programmers/level2/Solution2ByLv2.java
@@ -3,55 +3,40 @@ package org.programmers.level2;
 import java.util.*;
 
 public class Solution2ByLv2 {
-
+    static int lowestFatigue = Integer.MAX_VALUE;
+    static Map<String, Integer> pointTable = new HashMap<>();
     public int solution(int[] picks, String[] minerals) {
-        int answer = 0;
-
-        Map<Integer, List<String>> sumMineralPointsPerPickDescRank = new TreeMap<>(Comparator.reverseOrder());
-        Map<String, Integer> pointTable = new HashMap<>();
         pointTable.put("diamond", 25);
         pointTable.put("iron", 5);
         pointTable.put("stone", 1);
 
-        int sumMineralPointsPerPick = 0;
-        List<String> mineralsPerPickList = new ArrayList<>();
-        for(int i = 1; i <= minerals.length; i++) {
-            String mineralName = minerals[i - 1];
-
-            sumMineralPointsPerPick += pointTable.get(mineralName);
-            mineralsPerPickList.add(mineralName);
-
-            if(i % 5 == 0 || i == minerals.length) {
-                sumMineralPointsPerPickDescRank.put(sumMineralPointsPerPick, mineralsPerPickList);
-                sumMineralPointsPerPick = 0;
-                mineralsPerPickList = new ArrayList<>();
-            }
-        }
-
-        for (List<String> mineMineralList : sumMineralPointsPerPickDescRank.values()) {
-            int pickPoint = 1;
-            int index = -1;
-
-            boolean soldoutFlag = true;
-
-            for(int j = 0; j < picks.length; j++) {
-                if(picks[j] == 0) continue;
-
-                pickPoint = j == 0 ? 25 : j == 1 ? 5 : 1;
-                index = j;
-                soldoutFlag = false;
-                break;
-            }
-
-            if(soldoutFlag) return answer;
-
-            for(String mineMineralName : mineMineralList) {
-                int mineralPoint = pointTable.get(mineMineralName);
-                answer += Math.max(mineralPoint / pickPoint , 1);
-            }
-            picks[index]--;
-        }
-        return answer;
+        getLowestFatigue(picks, minerals, 0, 0);
+        return lowestFatigue;
     }
 
+    static public void getLowestFatigue(int[] picks, String[] minerals, int index, int currentFatigue) {
+        if(index > minerals.length -1 || picks[0] == 0 && picks[1] == 0 && picks[2] == 0) {
+            lowestFatigue = Math.min(lowestFatigue, currentFatigue);
+            return;
+        }
+
+        for(int i = 0; i < picks.length; i++) {
+            if(picks[i] == 0) continue;
+
+            String pickName = i == 0 ? "diamond" : i == 1 ? "iron" : "stone";
+            picks[i]--;
+            int originalFatigue = currentFatigue;
+
+            for(int j = index; j < 5 + index; j++) {
+                if(j == minerals.length) break;
+                int mineralPoint = pointTable.get(minerals[j]);
+                int pickPoint = pointTable.get(pickName);
+                currentFatigue += Math.max(mineralPoint / pickPoint , 1);
+            }
+
+            getLowestFatigue(picks, minerals, index + 5, currentFatigue);
+            currentFatigue = originalFatigue;
+            picks[i]++;
+        }
+    }
 }
