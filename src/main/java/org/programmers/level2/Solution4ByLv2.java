dde50ae8a5bce3a684e13fbb70dbfb7b042ff8ea
LaDiuM <wjddhrtjd123@gmail.com>
2024-07-15 10:45:27 +0900

    [LEVEL2 - 미로 탈출] 미로 탈출 BFS 사용하여 해결

--- src/main/java/org/programmers/level2/Solution4ByLv2.java
+++ src/main/java/org/programmers/level2/Solution4ByLv2.java
@@ -4,103 +4,86 @@ import java.util.*;
 
 public class Solution4ByLv2 {
 
-    // {
-    //      "S O O O L",
-    //      "X X X X O",
-    //      "O O O O O",
-    //      "O X X X X",
-    //      "O O O O E"
-    //  }
-    //  result 16
+    public static class Node {
+        int y, x, distance;
 
-    static String[][] maze;
-
-    public class Node {
-        int x;
-        int y;
-        List<int[]> history = new ArrayList<>();
-        Node(int x, int y) { this.x = x; this.y = y; }
+        Node(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
     }
 
-    // 상하좌우 확인용 벡터
+    static String[][] maze;
+    static boolean[][] visitedNodes;
+
     static final int[] dRow = {-1, 1, 0, 0};
     static final int[] dCol = {0, 0, -1, 1};
 
     public int solution(String[] maps) {
-        maze = new String[maps.length][maps[0].length()];
-        int[] startNode = new int[1];
-        int[] leverNode = new int[1];
-        int[] exitNode = new int[1];
+        int xLength = maps[0].length();
+        int yLength = maps.length;
 
-        for(int i = 0; i < maps.length; i++) {
+        maze = new String[yLength][xLength];
+        visitedNodes = new boolean[yLength][xLength];
+
+        int[] startNode = new int[2];
+        int[] leverNode = new int[2];
+
+        for (int i = 0; i < maps.length; i++) {
             String[] nodes = maps[i].split("");
-            for(int j = 0; j < nodes.length; j++) {
+            for (int j = 0; j < nodes.length; j++) {
                 String node = nodes[j];
-                if(node.equals("S")) startNode = new int[]{i, j};
-                if(node.equals("L")) leverNode = new int[]{i, j};
-                if(node.equals("E")) exitNode = new int[]{i, j};
+                if (node.equals("S")) startNode = new int[]{i, j};
+                if (node.equals("L")) leverNode = new int[]{i, j};
                 maze[i][j] = node;
             }
         }
-        System.out.println("maze = " + Arrays.deepToString(maze));
-        System.out.println("startNode = " + Arrays.toString(startNode));
-        System.out.println("startNode = " + Arrays.toString(leverNode));
-        System.out.println("startNode = " + Arrays.toString(exitNode));
 
         Queue<Node> queue = new LinkedList<>();
         queue.add(new Node(startNode[0], startNode[1]));
 
+        int leverDistance = getHistoryBfs("L", queue);
+        if(leverDistance == -1) return -1;
 
+        queue = new LinkedList<>();
+        queue.add(new Node(leverNode[0], leverNode[1]));
+        visitedNodes = new boolean[yLength][xLength];
 
-        return 1;
+        int leverToExitDistance = getHistoryBfs("E", queue);
+        if(leverToExitDistance == -1) return -1;
+
+        return leverDistance + leverToExitDistance;
     }
 
-    static List<int[]> bfs(String goal, String[] maps, Queue<Node> queue) {
-        Node current = queue.poll();
-        if(maze[current.x][current.y].equals(goal)) return current.history;
+    static int getHistoryBfs(String goal, Queue<Node> queue) {
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            int currentDistance = current.distance;
+            visitedNodes[current.y][current.x] = true;
+            if (getNodeType(current.y, current.x).equals(goal)) return current.distance;
 
-        return null;
-
+            for (int i = 0; i < 4; i++) {
+                checkNextNode(queue, current, dCol[i], dRow[i], currentDistance + 1);
+            }
+        }
+        return -1;
     }
-//
-//    static void initAdjList(int numVertices) {
-//        for(int i = 0; i < numVertices; i++) adjMaps[i] = new LinkedHashMap<>();
-//    }
-//
-//    static void checkMoveTop(int row, int col, int index) {
-//        if(row == 0) return;
-//        int adjRow = row - 1;
-//        String character = maze[adjRow].split("")[col];
-//        if(!character.equals("X")) {
-//            adjMaps[index].put(adjRow * maze[0].length() + col, character);
-//        }
-//    }
-//
-//    static void checkMoveRight(int row, int col, int index) {
-//        if(col == maze[0].length() - 1) return;
-//        int adjCol = col + 1;
-//        String character = maze[row].split("")[adjCol];
-//        if(!character.equals("X")) {
-//            adjMaps[index].put(row * maze[0].length() + adjCol, character);
-//        }
-//    }
-//
-//    static void checkMoveBottom(int row, int col, int index) {
-//        if(row == maze.length - 1) return;
-//        int adjRow = row + 1;
-//        String character = maze[adjRow].split("")[col];
-//        if(!character.equals("X")) {
-//            adjMaps[index].put(adjRow * maze[0].length() + col, character);
-//        }
-//    }
-//
-//    static void checkMoveLeft(int row, int col, int index) {
-//        if(col == 0) return;
-//        int adjCol = col - 1;
-//        String character = maze[row].split("")[adjCol];
-//        if(!character.equals("X")) {
-//            adjMaps[index].put(row * maze[0].length() + adjCol, character);
-//        }
-//    }
+
+    static String getNodeType(int y, int x) {
+        return maze[y][x];
+    }
+
+    static void checkNextNode(Queue<Node> queue, Node node, int dCol, int dRow, int newDistance) {
+        int x = node.x + dCol;
+        int y = node.y + dRow;
+        if(x < 0 || x > maze[0].length -1 || y < 0 || y > maze.length - 1 || visitedNodes[y][x]) return;
+
+        visitedNodes[y][x] = true;
+        Node nextNode = new Node(y, x);
+        if (!getNodeType(y, x).equals("X")) {
+            nextNode.distance = newDistance;
+            queue.add(nextNode);
+        }
+    }
 
 }
