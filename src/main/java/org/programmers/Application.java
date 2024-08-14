72e89ca7f7a71fa4be35faab11ebab99df1c5aef
LaDiuM <wjddhrtjd123@gmail.com>
2024-08-14 15:23:33 +0900

    [LEVEL3 - 등굣길] DP를 이용한 문제 해결, 1차 시도로 통과 후 단순 배열 반복으로 리팩토링 진행

--- src/main/java/org/programmers/Application.java
+++ src/main/java/org/programmers/Application.java
@@ -1,31 +1,65 @@
 package org.programmers;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Application {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
-        String[] strArr = scan.nextLine().split(" ");
-        int n = Integer.parseInt(strArr[0]);
-        int k = Integer.parseInt(strArr[1]);
+    public int solution(int n, int[][] computers) {
+        int[] parent = new int[n];
 
-        boolean[] checkArr = new boolean[n + 1];
+        // 초기화: 각 컴퓨터를 자기 자신을 루트로 설정
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
 
-        int count = 0;
-        for (int i = 2; i <= n; i++) {
-            if (!checkArr[i]) {
-                for (int j = i; j <= n; j += i) {
-                    if (!checkArr[j]) {
-                        checkArr[j] = true;
-                        count++;
-                        if (count == k) {
-                            System.out.println(j);
-                            return;
-                        }
-                    }
+        // 네트워크 병합
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (computers[i][j] == 1) {
+                    union(parent, i, j);
                 }
             }
         }
+
+        // 고유한 네트워크 수 계산
+        int numberOfNetworks = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                numberOfNetworks++;
+            }
+        }
+
+        return numberOfNetworks;
     }
+
+    // 루트 찾기
+    private int find(int[] parent, int node) {
+        if (parent[node] != node) {
+            parent[node] = find(parent, parent[node]); // 경로 압축
+        }
+        return parent[node];
+    }
+
+    // 두 노드 병합
+    private void union(int[] parent, int node1, int node2) {
+        int root1 = find(parent, node1);
+        int root2 = find(parent, node2);
+        if (root1 != root2) {
+            parent[root2] = root1; // 하나의 루트로 병합
+        }
+    }
+
+    public static void main(String[] args) {
+        Application application = new Application();
+
+        int n = 3;
+        int[][] computers = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+//        int n = 3;
+//        int[][] computers = {{1, 1, 1, 0}, {1, 1, 0, 0}, {1, 0, 1, 0}, {0, 0, 0, 1}};
+
+        int solution = application.solution(n, computers);
+        System.out.println(solution);
+
+    }
+
 }
